a9ddddc787f2f1ce52151694b655170457d6aa43
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-08-20 04:39:57 +0530

    Update settings.py

--- testcss/settings.py
+++ testcss/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
