9d6667b99c21d30fc3d7ad40bdad17ecdcd34ce5
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-08-20 16:25:26 +0530

    Update settings.py

--- testcss/settings.py
+++ testcss/settings.py
@@ -16,7 +16,6 @@ import os
 from urllib.parse import urlparse
 import mimetypes
 mimetypes.add_type("text/html", ".css", True)
-mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
