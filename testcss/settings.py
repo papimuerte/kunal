37463b560adf9add1b473a73fe91380a5e96f0da
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-08-20 15:17:09 +0530

    Update settings.py

--- testcss/settings.py
+++ testcss/settings.py
@@ -15,7 +15,7 @@ from django.core.management.utils import get_random_secret_key
 import os
 from urllib.parse import urlparse
 import mimetypes
-mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
